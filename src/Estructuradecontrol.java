803a961cb53d1070d4f2f454bdec925d1e6f57bf
Mario <marioenriquevasquez4@gmail.com>
2024-09-11 02:20:32 -0600

    Ejercicios_Clase

---
+++ src/Estructuradecontrol.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author TYT
+ */
+public class Estructuradecontrol {
+    public static void main(String[] args) {
+        int numero = 0;
+            while(numero<5){
+                numero=numero+2;
+                System.out.println(numero);
+                //break;
+        
+     }
+             
+    }
+    
+}
---
+++ src/FORECPLICACION.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import  java.util.Scanner;
+/**
+ *
+ * @author TYT
+ */
+public class FORECPLICACION {
+    public static void main(String[] args) {
+        Scanner lea = new Scanner(System.in);
+        
+       
+       
+     
+        
+        
+        
+        
+        
+        
+       
+    }
+    
+}
+ 
